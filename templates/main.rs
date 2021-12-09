d854ef12153c339a82e21c5cd27892b27aca0fa6
Alex <alex@ransomti.me>
2021-12-09 12:01:00 +0000

    add templates

---
+++ templates/main.rs
@@ -0,0 +1,25 @@
+mod inputs;
+fn main() {
+    println!("part 1: {}", part_1());
+    println!("part 2: {}", part_2());
+}
+
+fn part_1() -> i32 {
+    0
+}
+
+fn part_2() -> i32 {
+    0
+}
+
+
+
+#[cfg(test)]
+mod test {
+    use crate::inputs;
+
+    #[test]
+    fn dummy_test() {
+        assert_eq!(inputs::test, "");
+    }
+}
